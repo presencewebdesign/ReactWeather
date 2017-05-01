a14b3b4e0905c6ba99271d6da2487f25af2e0db9
James Stevenson <info@presencewebdesign.co.uk>
2017-05-01 16:34:51 +0100

    add in symbols for temperature format

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp,location}) => {
 	return(
-		<h3>It is {temp} in {location}</h3>
+		<h3><pre>It is {temp} &deg;C|&deg;F in {location}</pre></h3>
 	);
 }
 

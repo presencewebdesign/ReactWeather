2075ce72d38c551841188d90b3ee1f93473b2885
James Stevenson <info@presencewebdesign.co.uk>
2017-05-01 11:44:34 +0100

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
 	return(
-		<h3>Examples Components!!</h3>
+		<div>
+		<h3>Examples Components!</h3>
+		<p>Welcome to examples page</p>
+		</div>
 	);
 }
 
